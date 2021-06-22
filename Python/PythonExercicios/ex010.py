a04e63893f7bf73ccbfd3a04951effa6d842ed51
Ruth Anne <anne.carvalho@yahoo.com.br>
2021-06-21 22:49:12 -0300

    ex024

--- Python/PythonExercicios/ex010.py
+++ Python/PythonExercicios/ex010.py
@@ -1,4 +1,3 @@
-#quanto dinheiro tem na carteira e converter em dolar (5.09)
 carteira = float(input("Digite o valor que tem na carteira: R$"))
 dolar = carteira / 5.12
 print("Com R${:.2f}, você pode comprar US${:.2f}.".format(carteira, dolar))
